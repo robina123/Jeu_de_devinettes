5bab770351e9eea4c3f7fa5b1f500d050ad64f69
robina <robina@educ.college-montreal.qc.ca>
2022-10-25 09:12:19 -0400

    final faire les commentaires puis rendre :)

--- main.py
+++ main.py
@@ -1,25 +1,36 @@
+
+
 # arthur robin
 # 2022-09-04
 # Jeu_De_Devinette_2.0
+#J'importe random et je mets le nombre d'essai a zero
 import random
-
-
 nombreEssai = 0
-chiffre1 = int(input("Entrer le premier chiffre"))
-chiffre2 = int(input("Entrer le deuxieme chiffre"))
+#Je demande a 'utilisateur si il veut choisir ces chiffres ou non
+demander = input('voulez vous choisir vos chiffres? (oui/non)\n Si vous ne mettez pas le bon resultat vos chiffres seront choisi automatiquement!')
+
+if demander == 'oui':
+#Si l'utilisateur
+    nombreEssai = 0
+    chiffre1 = int(input("Entrer le premier chiffre"))
+    chiffre2 = int(input("Entrer le deuxieme chiffre"))
+else:
+    chiffre1 = 0
+    chiffre2 = 100
+    print('Le chiffre recherché est entre 0 et 100')
 chiffreRandom = random.randint(chiffre1, chiffre2)
 def Jeu_de_devinette():
     global nombreEssai
     fin = True
     while fin:
-
+        demanderRecommencer = False
         # choix de l'utilisateur
-        chiffreRentrer = int(input("Trouver le nombre recherché"))
+        chiffreRentrer = int(input("Trouver le nombre recherché\nQuel est votre réponse?"))
         if chiffreRentrer == chiffreRandom:
             # si le joueur reussi
             nombreEssai = nombreEssai + 1
             print("Bravo!\n", "Vous avez reussi en ", nombreEssai, "essais")
-            fin = False
+            demanderRecommencer = True
         elif chiffreRentrer < chiffreRandom:
             # si le chiffre est trop petitimport random
             print("Le chiffre est plus grand")
@@ -28,5 +39,14 @@ def Jeu_de_devinette():
             # si le chiffre est trop grand
             print("Le chiffre est plus petit")
             nombreEssai = nombreEssai + 1
+        if demanderRecommencer == True:
+            recommencer = input("voulez vous recommencez?")
+            if recommencer == "non":
+                print("Merci d'avoir joué")
+                exit()
+            elif recommencer == "oui":
+                break
 
 Jeu_de_devinette()
+
+
